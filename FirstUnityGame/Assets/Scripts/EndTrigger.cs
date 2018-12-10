d17a7280b044d73ca9cbf402efce06016875d612
Ahmed El Badry <ahmed2lbadry@gmail.com>
2018-12-10 06:37:09 +0200

    initial level

---
+++ FirstUnityGame/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+
+
+    void OnTriggerEnter()
+    {
+        FindObjectOfType<GameManager>().completeLevel();
+    }
+
+
+}
---
+++ FirstUnityGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("restart", restartDelay);
+        }
+
+
+    }
+
+    public void completeLevel()
+    {
+        Debug.Log("asdasd");
+        completeLevelUI.SetActive(true);
+    }
+    void restart()
+    {
+        //SceneManager.LoadScene("Level01");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameEnded = false;
+    }
+
+
+}
