e0739697d945811d1e8087c182b3aa0443c16007
Ahmed El Badry <ahmed2lbadry@gmail.com>
2018-12-10 03:42:36 +0200

    created walls, camera follows the player but needs optimaztions, shuold remove the acceleration of the player movement

---
+++ FirstUnityGame/Assets/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+    public Rigidbody rb;
+    public float movementForce;
+
+    public Vector3 v;
+
+
+    void FixedUpdate()
+    {
+        rb.velocity = v;
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, movementForce * Time.deltaTime, ForceMode.VelocityChange);
+            
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movementForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -movementForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movementForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        
+
+        /*if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameMananger>().EndGame();
+        }*/
+    }
+}
